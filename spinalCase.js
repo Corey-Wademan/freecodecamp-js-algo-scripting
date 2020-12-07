1f59c6f938a0a9ebbbc53c2182dac98de5c7581c
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2020-12-07 12:57:26 -0500

    Update spinalCase.js

--- spinalCase.js
+++ spinalCase.js
@@ -1,12 +1,16 @@
-function spinalCase(str) {
-    str.toLowerCase();
-    const regex = /\s|_|[A-Z]/gm;
-    
-    for (let x=0; x < str.length; x++) {
-        if (str[x] === regex) {
-           return str[x] === '-'
-        }
-    }
-    return str
-}  
-  console.log(spinalCase('This Is Spinal Tap'));
+// Converts a string to lowercase then places '-' between each word that are orginally seperated by underscores, spaces, or capital letters.
+// ----------Tests-----------
+// "This Is Spinal Tap" --> "this-is-spinal-tap"
+// "TheAndyGriffith_Show" --> "the-andy-griffith-show"
+// "AllThe-small Things" --> "all-the-small-things"
+
+
+
+
+function spinalCase(str) {    
+    const regex = /\s|_|(?=[A-Z])/gm;
+    return str 
+      .split(regex)
+      .join("-")
+      .toLowerCase();
+};
