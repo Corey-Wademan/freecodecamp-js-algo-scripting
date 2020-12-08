99f81eadfd0ddc65c96ebc11af173f233a83c754
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2020-12-08 11:41:06 -0500

    Update pigLatin.js

--- pigLatin.js
+++ pigLatin.js
@@ -1,3 +1,9 @@
+// If a word begins with a consonant, take the first consonant or consonant cluster, move it to the end of the word, and add "ay" to it.
+// If a word begins with a vowel, just add "way" at the end.
+
+// ex.  'california' should return "aliforniacay" | "algorithm" should return "algorithmway" | "schwartz" should return "artzschway"
+
+
 function translatePigLatin(str) {
     const consonant = /[bcdfghjklmnpqrstvwxyz]/gymi;
     const vowel = /[aeiou]/mi;
