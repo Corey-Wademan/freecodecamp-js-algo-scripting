64e7e02dba63beeca0ecec497bb809ffec1f74e4
Corey Wademan <67563215+Corey-Wademan@users.noreply.github.com>
2020-12-12 17:07:51 -0500

    Update SearchAndReplace.js

--- SearchAndReplace.js
+++ SearchAndReplace.js
@@ -1,3 +1,7 @@
+// myReplace("Let us go to the store", "store", "mall") should return "Let us go to the mall".
+// myReplace("He is Sleeping on the couch", "Sleeping", "sitting") should return "He is Sitting on the couch".
+// myReplace("His name is Tom", "Tom", "john") should return "His name is John".
+
 function myReplace(str, before, after) {
     const index = str.indexOf(before);
 
